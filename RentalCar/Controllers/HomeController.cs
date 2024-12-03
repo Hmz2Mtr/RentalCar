57a3083b3c85ea7c7f2373a9f389393893140937
Hmz2Mtr <hamzamtr13@gmail.com>
2024-12-03 21:36:03 +0100

    Added About Page.

--- RentalCar/Controllers/HomeController.cs
+++ RentalCar/Controllers/HomeController.cs
@@ -18,6 +18,12 @@ namespace RentalCar.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
+
         public IActionResult Privacy()
         {
             return View();
