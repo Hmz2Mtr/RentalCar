4f94f2c636bbbb0e00224159df7b818df188dd36
Hmz2Mtr <hamzamtr13@gmail.com>
2024-12-04 12:27:53 +0100

    Added User rep, BookedCars methode in UserController.

---
+++ RentalCar/Controllers/UserController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RentalCar.Controllers
+{
+    [Authorize]
+    public class UserController : Controller
+    {
+        public IActionResult BookedCars()
+        {
+            return View();
+        }
+    }
+}
