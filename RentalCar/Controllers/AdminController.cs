b7756951d3abff1ce6fcc6b0d2663b282e1a9ae3
Hmz2Mtr <hamzamtr13@gmail.com>
2025-01-20 18:29:41 +0100

    Added Dashboard Page for the admin.

--- RentalCar/Controllers/AdminController.cs
+++ RentalCar/Controllers/AdminController.cs
@@ -3,12 +3,13 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace RentalCar.Controllers
 {
-    [Authorize(Roles = "admin")]
+    [Authorize(Roles = "admin")] // Restrict access to users with the "Admin" role
     public class AdminController : Controller
     {
-        public IActionResult Display()
+        public IActionResult Dashboard()
         {
             return View();
         }
+
     }
 }
