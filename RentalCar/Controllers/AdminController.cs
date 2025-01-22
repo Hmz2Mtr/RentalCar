480f29e3e1863424e8bc8a1c67fafbd580cdba90
Hmz2Mtr <hamzamtr13@gmail.com>
2025-01-22 02:50:40 +0100

    Added booking Pages for the Admin.

--- RentalCar/Controllers/AdminController.cs
+++ RentalCar/Controllers/AdminController.cs
@@ -1,14 +1,88 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RentalCar.Data;
+using RentalCar.Models.Domain;
 
 namespace RentalCar.Controllers
 {
     [Authorize(Roles = "admin")] // Restrict access to users with the "Admin" role
     public class AdminController : Controller
     {
+        
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly DatabaseContext _context;
+
+        public AdminController(UserManager<ApplicationUser> userManager, DatabaseContext context)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+
+
         public IActionResult Dashboard()
         {
             return View();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+        // GET: /Booking/Index
+        public async Task<IActionResult> AllBookedCars()
+        {
+            var bookings = await _context.Bookings
+                .ToListAsync();
+
+            return View(bookings);
+        }
+
+
+
+
+
+        // GET: /Booking/Delete/{id}
+        public async Task<IActionResult> DeleteBooking(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var booking = await _context.Bookings
+                .FirstOrDefaultAsync(b => b.BookingID == id);
+
+            if (booking == null)
+            {
+                return NotFound();
+            }
+
+            return View(booking);
+        }
+
+        // POST: /Booking/Delete/{id}
+        [HttpPost, ActionName("DeleteBooking")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int? id)
+        {
+            var booking = await _context.Bookings.FindAsync(id);
+            if (booking == null)
+            {
+                return NotFound();
+            }
+
+            _context.Bookings.Remove(booking);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("AllBookedCars", "Admin");
         }
 
     }
