481a90691c0b22e7057ff5d12df8a2cbe1fc48c2
Hmz2Mtr <hamzamtr13@gmail.com>
2025-01-22 10:13:36 +0100

    Added Remaining bookings for the admin.

--- RentalCar/Controllers/AdminController.cs
+++ RentalCar/Controllers/AdminController.cs
@@ -46,10 +46,6 @@ namespace RentalCar.Controllers
             return View(bookings);
         }
 
-
-
-
-
         // GET: /Booking/Delete/{id}
         public async Task<IActionResult> DeleteBooking(int? id)
         {
@@ -83,6 +79,72 @@ namespace RentalCar.Controllers
             _context.Bookings.Remove(booking);
             await _context.SaveChangesAsync();
             return RedirectToAction("AllBookedCars", "Admin");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // GET: /Booking/Index
+        public async Task<IActionResult> RemainingBookedCars()
+        {
+            // Get the current date as DateOnly
+            DateOnly currentDate = DateOnly.FromDateTime(DateTime.Today);
+
+            var bookings = await _context.Bookings
+                .Where(b => b.EndDate >= currentDate)
+                .ToListAsync();
+
+            return View(bookings);
+        }
+
+
+
+        // GET: /Booking/Delete/{id}
+        public async Task<IActionResult> DeleteRemainingBooking(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var booking = await _context.Bookings
+                .FirstOrDefaultAsync(b => b.BookingID == id);
+
+            if (booking == null)
+            {
+                return NotFound();
+            }
+
+            return View(booking);
+        }
+
+        // POST: /Booking/Delete/{id}
+        [HttpPost, ActionName("DeleteRemainingBooking")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteRemainingConfirmed(int? id)
+        {
+            var booking = await _context.Bookings.FindAsync(id);
+            if (booking == null)
+            {
+                return NotFound();
+            }
+
+            _context.Bookings.Remove(booking);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("RemainingBookedCars", "Admin");
         }
 
     }
